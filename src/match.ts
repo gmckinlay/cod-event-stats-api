09e8c94276f438a8c664e55df0ddb3c0ca890c56
Graham McKinlay <3449059+gmckinlay@users.noreply.github.com>
2020-11-07 19:35:15 +0000

    Adding support for match data for tournaments

---
+++ src/match.ts
@@ -0,0 +1,13 @@
+import { Killstreak } from "@stagg/callofduty/lib/schema";
+
+export class Match {
+
+    constructor (
+        public matchId: string,
+        public startTime: number,
+        public mode: string,
+        public team: string,
+        public position: number,
+        public totalKills: number,
+        public totalDeaths:number){}
+}
